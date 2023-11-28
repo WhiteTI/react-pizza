ce58a73bd608796f19116bfa8b85db4454449e1f
white <example@gmail.ru>
2023-11-28 21:40:03 +0300

    add scss, create Header, Categories, Sort, PizzaBlock components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import "./scss/app.scss";
+import Header from "./components/Header/Header.jsx";
+import Categories from "./components/Categories/Categories.jsx";
+import Sort from "./components/Sort/Sort.jsx";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock.jsx";
 
 function App() {
-  return <div>123</div>;
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        <PizzaBlock title="Мексиканская" price={500} />
+                        <PizzaBlock title="Чизбургер-пицца" price={395} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
